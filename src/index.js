7cc8aca2e69180a9e3c957c2676115c643c01fba
Wev-Dev-Bibhu <iambibhutidas@gmail.com>
2026-02-18 11:21:57 +0530

    bugfix: Fix folder casing: Apis → apis v2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { SnackbarProvider } from 'notistack';
-import { AuthProvider } from './apis/AuthContext';
+import { AuthProvider } from './Apis/AuthContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
